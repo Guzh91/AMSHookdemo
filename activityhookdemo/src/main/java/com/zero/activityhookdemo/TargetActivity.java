11a087044ed556c26ef7faea0060177ce8ff53cc
yzp <1048023269@qq.com>
2021-02-26 19:54:32 +0800

    AMS、Binder

---
+++ activityhookdemo/src/main/java/com/zero/activityhookdemo/TargetActivity.java
@@ -0,0 +1,31 @@
+package com.zero.activityhookdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * TargetActivity并没有在AndroidManifest.xml中注册，但是依旧可以启动，
+ * 用的就是hook技术，主要原理就是反射
+ *
+ */
+public class TargetActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_show)
+    TextView tvShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_target);
+        ButterKnife.bind(this);
+
+
+    }
+
+
+}
